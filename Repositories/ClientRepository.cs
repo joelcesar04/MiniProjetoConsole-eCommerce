2a03b182de3c5e2cb7992d5444b35ee16bacef3b
Joel Andrade <joelcesar09@gmail.com>
2023-12-08 13:51:44 -0300

    Adicionado o repositório da tela de cliente.

---
+++ Repositories/ClientRepository.cs
@@ -0,0 +1,55 @@
+﻿using eCommerce.Console.Database;
+using eCommerce.Console.Models;
+using eCommerce.Console.Screens.UserScreen;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Console.Repositories
+{
+    public class ClientRepository : IRepository<Cliente>
+    {
+        private readonly eCommerceContext _db;
+        public ClientRepository(eCommerceContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Cliente> Get()
+        {
+            return _db.Clientes
+                .Include(cont => cont.Contato)
+                .Include(end => end.Endereco)
+                .ToList();
+        }
+
+        public Cliente Get(int id)
+        {
+            return _db.Clientes
+                .Include(cont => cont.Contato)
+                .Include(end => end.Endereco)
+                .FirstOrDefault(a => a.ClienteId == id)!;
+        }
+
+        public void Create(Cliente cliente)
+        {
+            _db.Clientes.Add(cliente);
+            _db.SaveChanges();
+        }
+        public void Update(Cliente cliente)
+        {
+            _db.Update(cliente);
+            _db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            _db.Remove(id);
+            _db.SaveChanges();
+        }
+
+    }
+}
---
+++ Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Console.Repositories
+{
+    public interface IRepository<T> 
+        where T : class
+    {
+        IEnumerable<T> Get();
+        T Get(int id);
+        void Create(T model);
+        void Update(T model);
+        void Delete(int id);
+    }
+}
