757d1ad6089f185ab81bfbccec1d8f7ab1187d1f
Joel Andrade <joelcesar09@gmail.com>
2024-01-08 13:25:28 -0300

    Atualização na Categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -9,7 +9,6 @@ namespace eCommerce.Console.Models
     public class Categoria
     {
         public int CategoriaId { get; set; }
-        public int ProdutoId { get; set; }
         public string Nome { get; set; } = null!;
         public string? Descricao { get; set; }
         public ICollection<Produto>? Produtos { get; set; }
